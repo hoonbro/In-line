1df6c1bf17caa1b735e0ab9c745f2c54bfb58af5
Jihoon Byun <wlgns1588@naver.com>
2021-07-22 22:36:53 +0900

    로그 추가

--- Backend/src/main/java/com/inline/sub2/api/controller/OfficeController.java
+++ Backend/src/main/java/com/inline/sub2/api/controller/OfficeController.java
@@ -1,7 +1,6 @@
 package com.inline.sub2.api.controller;
 
 import com.inline.sub2.api.dto.AdminRegistDto;
-import com.inline.sub2.api.dto.UserDto;
 import com.inline.sub2.api.service.DeptService;
 import com.inline.sub2.api.service.JobService;
 import com.inline.sub2.api.service.OfficeService;
@@ -10,11 +9,15 @@ import com.inline.sub2.db.entity.DeptEntity;
 import com.inline.sub2.db.entity.JobEntity;
 import com.inline.sub2.db.entity.OfficeEntity;
 import com.inline.sub2.db.entity.UserEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/office")
 @CrossOrigin("*")
@@ -35,23 +38,29 @@ public class OfficeController {
     @PostMapping()
     public ResponseEntity<Void> registUser(@RequestBody AdminRegistDto admin) {
         HttpStatus httpStatus = HttpStatus.OK;
-        //회사 등록
         try {
-            OfficeEntity officeEntity = officeService.registOffice(admin.getOfficeName());
-            DeptEntity deptEntity = deptService.getDeptId(admin.getDeptName());
-            JobEntity jobEntity = jobService.getJobId(admin.getJobName());
+            OfficeEntity officeEntity = officeService.registOffice(admin.getOfficeName()); //회사 등록
+            log.info("회사 등록 완료");
+            DeptEntity deptEntity = deptService.getDeptId(admin.getDeptName()); //부서 번호 조회
+            JobEntity jobEntity = jobService.getJobId(admin.getJobName()); //직책 번호 조회
+
             admin.setOfficeId(officeEntity.getOfficeId());
             admin.setDeptId(deptEntity.getDeptId());
             admin.setJobId(jobEntity.getJobId());
+
             //임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
             UserEntity userEntity = userService.registAdmin(admin);
             if (userEntity != null) {
                 httpStatus = HttpStatus.CREATED;
+            }else{
+                log.error("회원 등록 실패");
+                httpStatus = HttpStatus.BAD_REQUEST;
             }
         } catch (Exception e) {
+            log.error("회사 등록 실패 : {}", e);
             httpStatus = HttpStatus.BAD_REQUEST;
         }
-        
+
         return new ResponseEntity<Void>(httpStatus);
     }
 }
