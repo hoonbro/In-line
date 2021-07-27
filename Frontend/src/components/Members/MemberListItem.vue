6ddb5295bce0de9fbfaeaa5aa815756988d9ea29
byunghun-jake <figma@kakao.com>
2021-07-27 15:08:11 +0900

    Feat: Room 레이아웃

---
+++ Frontend/src/components/Members/MemberListItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <li class="user-list-item">
+    <div class="icon">
+      <span>Icon</span>
+    </div>
+    <div class="infos">
+      <p class="name">{{ member.name }}</p>
+      <p>{{ member.department }}</p>
+      <p>{{ member.position }}</p>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "MemberListItem",
+  props: {
+    member: Object,
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.user-list-item {
+  @apply flex items-center p-4;
+
+  &:hover {
+    @apply bg-gray-100 cursor-pointer select-none;
+  }
+
+  .icon {
+    @apply w-10 h-10 rounded-full bg-green-400 flex items-center justify-center text-sm font-bold mr-8 text-white;
+  }
+
+  .infos {
+    @apply flex items-center text-sm text-gray-400;
+
+    .name {
+      @apply text-base text-gray-900;
+    }
+
+    p:not(:last-child) {
+      @apply mr-2;
+    }
+  }
+}
+</style>
