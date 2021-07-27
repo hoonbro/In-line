df6ce34c16ce374211ab53a0070b490559331e8b
leeww0598 <leeww0598@gmail.com>
2021-07-27 16:46:41 +0900

    Update: Room layout

--- Frontend/src/components/Video.vue
+++ Frontend/src/components/Video.vue
@@ -1,19 +1,37 @@
 <template>
   <div>
-    <div class="face" v-if="videoList <= 9">
-      <img
+    <div class="face" v-if="videoList > 16">
+      <div class="picture"></div>
+      <!-- <img
         src="http://placeimg.com/400/240/any?t=1627265125999"
         alt=""
         class="picture"
-      />
+      /> -->
     </div>
-    <!-- <div class="face2" v-if="videoList > 9">
-      <img
+    <div class="face2" v-else-if="videoList > 9">
+      <div class="picture2"></div>
+      <!-- <img
         src="http://placeimg.com/400/240/any?t=1627265125999"
         alt=""
-        class="picture"
-      />
-    </div> -->
+        class="picture2"
+      /> -->
+    </div>
+    <div class="face3" v-else-if="videoList > 4">
+      <div class="picture3"></div>
+      <!-- <img
+        src="http://placeimg.com/400/240/any?t=1627265125999"
+        alt=""
+        class="picture3"
+      /> -->
+    </div>
+    <div class="face4" v-else>
+      <div class="picture4"></div>
+      <!-- <img
+        src="http://placeimg.com/400/240/any?t=1627265125999"
+        alt=""
+        class="picture4"
+      /> -->
+    </div>
   </div>
 </template>
 
@@ -29,26 +47,44 @@ export default {
 </script>
 
 <style scoped lang="scss">
-// @apply grid grid-rows-3 grid-cols-1;
-.face {
-  // @apply grid auto-cols-fr object-scale-down;
-  width: 400px;
-  height: 240px;
-  /* border-radius: 15px; */
-}
+// @apply grid auto-cols-fr;
+// .face {
+//   @apply w-64 h-36;
+// }
 
-.face2 {
-  width: 300px;
-  height: 180px;
-  display: flex;
-  /* border: 5px solid skyblue; */
-  /* border-radius: 15px; */
-}
+// .face2 {
+//   @apply w-48 h-28;
+// }
+
+// .face2 {
+//   @apply w-32 h-16;
+// }
 
 .picture {
   @apply rounded-2xl;
-  width: 395px;
-  height: 230px;
-  /* border-radius: 10px; */
+  width: 336px;
+  height: 189px;
+  border: 3px solid white;
+}
+
+.picture2 {
+  @apply rounded-2xl;
+  width: 416px;
+  height: 234px;
+  border: 3px solid white;
+}
+
+.picture3 {
+  @apply rounded-2xl;
+  width: 560px;
+  height: 315px;
+  border: 3px solid white;
+}
+
+.picture4 {
+  @apply rounded-2xl;
+  width: 800px;
+  height: 450px;
+  border: 3px solid white;
 }
 </style>
--- Frontend/src/views/Room.vue
+++ Frontend/src/views/Room.vue
@@ -1,24 +1,35 @@
 <template>
   <div class="wrapper">
-    <div class="videoChat">
-      <div class="videoPart">
+    <div class="video-chat">
+      <!-- ------------------------------------------------------------------------- -->
+      <div class="video-part" v-if="videoList > 16">
         <Video v-for="video in videoList" :key="video" :videoList="videoList" />
       </div>
-      <div class="barPart">
-        <div class="micButton">
+      <div class="video-part2" v-else-if="videoList > 9">
+        <Video v-for="video in videoList" :key="video" :videoList="videoList" />
+      </div>
+      <div class="video-part3" v-else-if="videoList > 4">
+        <Video v-for="video in videoList" :key="video" :videoList="videoList" />
+      </div>
+      <div class="video-part4" v-else>
+        <Video v-for="video in videoList" :key="video" :videoList="videoList" />
+      </div>
+      <!-- ------------------------------------------------------------------------- -->
+      <div class="bar-part">
+        <div class="mic-button">
           <span class="material-icons"> mic </span> 마이크 끄기
         </div>
-        <div class="camButton">
+        <div class="cam-button">
           <span class="material-icons"> videocam </span> 카메라 끄기
         </div>
-        <div class="closeButton">
+        <div class="close-button">
           <router-link :to="{ name: 'Office' }"><span>X</span></router-link>
         </div>
       </div>
     </div>
 
-    <div class="sidebar">
-      <div class="iconList">
+    <div class="side-bar">
+      <div class="icon-list">
         <ul>
           <li
             :class="{ active: activeMenu === 'roomChat' }"
@@ -73,10 +84,10 @@
           </li>
         </ul>
       </div>
-      <div class="chatList" v-if="activeMenu === 'officeChat'">
+      <div class="chat-list" v-if="activeMenu === 'officeChat'">
         채팅 목록입니다
       </div>
-      <div class="peopleList" v-if="activeMenu === 'people'">
+      <div class="people-list" v-if="activeMenu === 'people'">
         사람 목록입니다
       </div>
     </div>
@@ -93,12 +104,13 @@ export default {
     Video,
   },
   setup() {
-    const videoList = 6
+    const videoList = 16
     const activeMenu = ref("")
     // 처음에 아무것도 활성화되어 있지 않아도 되므로 공백으로 남겨둠
     const changeActiveMenu = menuTitle => {
       activeMenu.value = menuTitle
     }
+
     // 마이크 끄기, 카메라 끄기 버튼 눌렀을 때, 아이콘 전환되게 만들기
 
     return {
@@ -112,52 +124,60 @@ export default {
 
 <style scoped lang="scss">
 .wrapper {
-  @apply h-screen flex bg-purple-500 border-yellow-400 border-2;
-}
+  @apply h-full flex bg-purple-500 border-yellow-400 border-2;
 
-.videoChat {
-  @apply grid;
-}
+  .video-chat {
+    @apply border-red-500 border-2 flex-1;
 
-.videoPart {
-  @apply flex-1 grid grid-cols-3 m-28;
-}
+    .video-part {
+      @apply grid grid-cols-5 m-12 bg-Teal-300;
+    }
+    .video-part2 {
+      @apply grid grid-cols-4 m-12 bg-red-500;
+    }
+    .video-part3 {
+      @apply grid grid-cols-3 m-12 bg-yellow-300;
+    }
+    .video-part4 {
+      @apply grid grid-cols-2 m-12 bg-black;
+    }
 
-.barPart {
-  @apply flex justify-center align-middle;
+    .bar-part {
+      @apply flex fixed left-1/3 bottom-10;
 
-  .micButton {
-    @apply flex bg-blue-900 rounded-full h-10 w-36 text-white justify-center mx-2 place-items-center cursor-pointer;
-  }
+      .mic-button {
+        @apply flex bg-blue-900 rounded-full h-10 w-36 text-white justify-center mx-2 place-items-center cursor-pointer;
+      }
 
-  .camButton {
-    @apply flex bg-blue-900 rounded-full h-10 w-36 text-white justify-center mx-2 place-items-center cursor-pointer;
-  }
+      .cam-button {
+        @apply flex bg-blue-900 rounded-full h-10 w-36 text-white justify-center mx-2 place-items-center cursor-pointer;
+      }
 
-  .closeButton {
-    @apply flex bg-red-600 rounded-full h-10 w-10 text-white justify-center mx-2 place-items-center;
-  }
-}
-
-.sidebar {
-  @apply flex flex-row-reverse w-96 h-screen ml-auto border-yellow-400 border-2;
-
-  .chatList {
-    @apply flex bg-white w-full h-full min-w-full;
-    transition: width 0.5s;
-  }
-
-  .peopleList {
-    @apply flex bg-white w-full h-full min-w-full;
-  }
-  .iconList {
-    border: 1px solid gray;
-    .icon {
-      @apply p-4 inline-block justify-center cursor-pointer;
+      .close-button {
+        @apply flex bg-red-600 rounded-full h-10 w-10 text-white justify-center mx-2 place-items-center;
+      }
     }
   }
-}
-.sidebar ul li.active {
-  @apply text-blue-700;
+
+  .side-bar {
+    @apply flex flex-row-reverse w-16 h-screen ml-auto border-yellow-400 border-2;
+
+    .chat-list {
+      @apply bg-white w-full h-full;
+    }
+
+    .people-list {
+      @apply bg-white w-full h-full;
+    }
+    .icon-list {
+      border: 1px solid gray;
+      .icon {
+        @apply p-4 inline-block justify-center cursor-pointer;
+      }
+    }
+  }
+  .side-bar ul li.active {
+    @apply text-blue-700;
+  }
 }
 </style>
