5da5dc7ee87ac1c97354e98cc84ca7667bc82062
최영수 <soc4585@gmail.com>
2021-07-28 15:03:07 +0900

    Feat: 회사등록/로그인 유효성 검사 및 로그인 로그아웃 라우팅

--- Frontend/src/components/RegistOfficeModal.vue
+++ Frontend/src/components/RegistOfficeModal.vue
@@ -11,71 +11,44 @@
       </div>
       <div class="input-list">
         <TextInput
+          v-for="(field, key) in formData"
+          :key="key"
+          :name="key"
+          :label="field.label"
+          v-model="field.value"
+          :type="field.type"
+          :errors="field.errors"
+          :validators="field.validators"
+        />
+        <!-- <TextInput
           v-model="formData.officeName"
           label="회사 이름"
           type="text"
-          :required="true"
-          :shouldEmitEvent="true"
-          @blur="validation($event, 'officeName')"
         />
         <hr />
-        <TextInput
-          v-model="formData.email"
-          label="이메일"
-          type="email"
-          :shouldEmitEvent="true"
-          @blur="validation($event, 'email')"
-        />
-        <TextInput
-          v-model="formData.deptName"
-          label="소속"
-          type="text"
-          :shouldEmitEvent="true"
-          @blur="validation($event, 'deptName')"
-        />
-        <TextInput
-          v-model="formData.jobname"
-          label="직무"
-          type="text"
-          :shouldEmitEvent="true"
-          @blur="validation($event, 'jobName')"
-        />
-        <TextInput
-          v-model="formData.name"
-          label="이름"
-          type="text"
-          :shouldEmitEvent="true"
-          @blur="validation($event, 'name')"
-        />
-        <TextInput
-          v-model="formData.phone"
-          label="휴대전화"
-          type="text"
-          :shouldEmitEvent="true"
-          @blur="validation($event, 'phone')"
-        />
+        <TextInput v-model="formData.email" label="이메일" type="email" />
+        <TextInput v-model="formData.deptName" label="소속" type="text" />
+        <TextInput v-model="formData.jobName" label="직무" type="text" />
+        <TextInput v-model="formData.name" label="이름" type="text" />
+        <TextInput v-model="formData.phone" label="휴대전화" type="text" />
         <hr />
         <TextInput
           v-model="formData.password"
           label="비밀번호"
           type="password"
-          :shouldEmitEvent="true"
-          @blur="validation($event, 'password')"
         />
         <TextInput
-          v-model="formData.password2"
+          v-model="formData.confirmPassword"
           label="비밀번호 확인"
           type="password"
-          :shouldEmitEvent="true"
-          @blur="validation($event, 'password2')"
-        />
+        /> -->
         <div>
           <input
             class="mr-1"
             type="checkbox"
             name="term"
             id="term"
-            v-model="formData.term"
+            v-model="term"
           />
           <label class="text-sm font-medium" for="term"
             >이용약관 및 개인정보처리방침 동의</label
@@ -91,7 +64,7 @@
 
 <script>
 import TextInput from "@/components/TextInput.vue"
-import { reactive } from "vue"
+import { reactive, ref } from "vue"
 import { useStore } from "vuex"
 
 export default {
@@ -100,46 +73,126 @@ export default {
   setup(props, { emit }) {
     const store = useStore()
 
-    const formData = reactive({
-      officeName: "",
-      email: "",
-      deptName: "",
-      jobname: "",
-      name: "",
-      phone: "",
-      password: "",
-      password2: "",
-      term: false,
-    })
-
-    const registerOffice = async () => {
-      const res = await store.dispatch("landing/registerOffice", formData)
-      console.log("res: ", res)
-      emit("close")
+    const requiredValidator = key => {
+      if (formData[key].value < 1) {
+        formData[key].errors.required = "필수 입력 요소입니다."
+        return false
+      }
+      delete formData[key].errors.required
+      return true
     }
 
-    const validation = (event, field) => {
-      const data = event.target.value
-      // 유효성 검사
-      // 1. 필수 입력
-      //  - 모두
-      if (!data) {
-        // tag를 빨간색으로 바꾸고, 밑에 안내 메시지 보여주기
-        //
+    const emailValidator = key => {
+      if (
+        !/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/.test(
+          formData[key].value
+        )
+      ) {
+        formData[key].errors.invalidEmail = "올바른 이메일 주소를 입력해주세요."
+        return false
       }
-      // 2. 중복검사
-      //  - officeName, email
-      //    -> rest API로 검사 요청 (DB TABLE의 해당 field에 해당 data가 존재하는지)
-      if (field === "officeName" || field === "email") {
-        console.log("중복검사를 해야합니다.")
+      delete formData[key].errors.invalidEmail
+      return true
+    }
+
+    const passwordSecurityValidator = key => {
+      if (
+        !/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/.test(
+          formData[key].value
+        )
+      ) {
+        formData[key].errors.weekPassword =
+          "대소문자, 숫자, 특수문자 조합으로 8자리 이상으로 작성하세요."
+        return false
       }
+      delete formData[key].errors.weekPassword
+      return true
+    }
+
+    const confirmPasswordValidator = key => {
+      if (formData[key].value !== formData.password.value) {
+        formData[key].errors.notMatch = "비밀번호가 일치하지 않습니다."
+        return false
+      }
+      delete formData[key].errors.notMatch
+      return true
+    }
+
+    const formData = reactive({
+      officeName: {
+        label: "회사 이름",
+        type: "text",
+        value: "",
+        validators: [requiredValidator],
+        errors: {},
+      },
+      email: {
+        label: "이메일",
+        type: "email",
+        value: "",
+        validators: [requiredValidator, emailValidator],
+        errors: {},
+      },
+      deptName: {
+        label: "소속",
+        type: "text",
+        value: "",
+        validators: [requiredValidator],
+        errors: {},
+      },
+      jobName: {
+        label: "직무",
+        type: "text",
+        value: "",
+        validators: [requiredValidator],
+        errors: {},
+      },
+      name: {
+        label: "이름",
+        type: "text",
+        value: "",
+        validators: [requiredValidator],
+        errors: {},
+      },
+      phone: {
+        label: "휴대전화",
+        type: "text",
+        value: "",
+        validators: [requiredValidator],
+        errors: {},
+      },
+      password: {
+        label: "비밀번호",
+        type: "password",
+        value: "",
+        validators: [requiredValidator, passwordSecurityValidator],
+        errors: {},
+      },
+      confirmPassword: {
+        label: "비밀번호 확인",
+        type: "password",
+        value: "",
+        validators: [requiredValidator, confirmPasswordValidator],
+        errors: {},
+      },
+    })
+
+    const term = ref(false)
+
+    const registerOffice = async () => {
+      const submitData = { term: term.value }
+      Object.keys(formData).forEach(
+        key => (submitData[key] = formData[key].value)
+      )
+      await store.dispatch("landing/registerOffice", submitData)
+      emit("close")
     }
 
     return {
       store,
       formData,
+      term,
       registerOffice,
-      validation,
     }
   },
 }
