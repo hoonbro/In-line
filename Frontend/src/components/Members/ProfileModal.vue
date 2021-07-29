73c4e56633e58996a786aff787496a7f1c84ac67
byunghun-jake <figma@kakao.com>
2021-07-29 10:34:31 +0900

    Feat: 프로필 모달 구현

--- Frontend/src/components/Members/ProfileModal.vue
+++ Frontend/src/components/Members/ProfileModal.vue
@@ -1,58 +1,46 @@
 <template>
   <Modal>
     <template v-slot:modal-header>
-      <header class="border-b border-gray-300 flex p-2 justify-end">
-        <button @click="$emit('close')" class="p-2 w-10 h-10">
+      <header>
+        <button @click="$emit('close')">
           <span class="material-icons">close</span>
         </button>
       </header>
     </template>
     <template v-slot:modal-body>
-      <div class="mb-10 flex justify-center">
-        <div class="bg-green-200 rounded-full w-32 h-32"></div>
+      <div class="modal-body">
+        <div class="profile-img-wrapper">
+          <span>
+            {{ profile.name.value }}
+          </span>
+        </div>
       </div>
       <div>
-        <div class="flex items-center justify-between mb-4">
-          <h3 class="text-xl font-bold">기본정보</h3>
-          <button class="p-1 w-8 h-8" v-if="true">
-            <span class="material-icons text-gray-500">edit</span>
+        <div class="header">
+          <h3>기본정보</h3>
+          <button class="edit-btn" v-if="true">
+            <span class="material-icons ">edit</span>
           </button>
         </div>
-        <ul class="grid">
-          <li class="flex items-center py-4 border-b">
-            <p class="font-medium text-gray-400 w-40">부서</p>
-            <p class="">FE</p>
-          </li>
-          <li class="flex items-center py-4 border-b">
-            <p class="font-medium text-gray-400 w-40">직무</p>
-            <p class="">팀원</p>
-          </li>
-          <li class="flex items-center py-4 border-b">
-            <p class="font-medium text-gray-400 w-40">이메일</p>
-            <p class="">figma@kakao.com</p>
-          </li>
-          <li class="flex items-center py-4 border-b">
-            <p class="font-medium text-gray-400 w-40">이름</p>
-            <p class="">김병훈</p>
-          </li>
-          <li class="flex items-center py-4 border-b">
-            <p class="font-medium text-gray-400 w-40">닉네임</p>
-            <p class="">Vue</p>
-          </li>
-          <li class="flex items-center py-4 border-b">
-            <p class="font-medium text-gray-400 w-40">휴대전화</p>
-            <p class="">010-3294-3270</p>
+        <ul class="info-list">
+          <li class="info-list-item" v-for="(field, key) in profile" :key="key">
+            <p class="label">{{ field.label }}</p>
+            <p class="">{{ field.value }}</p>
           </li>
         </ul>
       </div>
-      <div class="mt-4 flex justify-end" v-if="true">
-        <button class="font-medium text-gray-600">비밀번호 변경</button>
+      <div class="change-pwd-btn-container" v-if="true">
+        <button>비밀번호 변경</button>
       </div>
     </template>
   </Modal>
 </template>
 
 <script>
+import { reactive } from "@vue/reactivity"
+import { onMounted } from "@vue/runtime-core"
+import axios from "axios"
+
 import Modal from "@/components/Common/Modal.vue"
 
 export default {
@@ -60,7 +48,106 @@ export default {
   components: {
     Modal,
   },
+  props: {
+    userId: Number,
+  },
+  setup(props) {
+    const profile = reactive({
+      department: {
+        label: "부서",
+        value: "",
+      },
+      position: {
+        label: "직무",
+        value: "",
+      },
+      email: {
+        label: "이메일",
+        value: "",
+      },
+      name: {
+        label: "이름",
+        value: "",
+      },
+      nickName: {
+        label: "닉네임",
+        value: "",
+      },
+      phone: {
+        label: "휴대전화",
+        value: "",
+      },
+    })
+
+    onMounted(async () => {
+      const res = await axios.get(`http://localhost:3000/users/${props.userId}`)
+      console.log(res)
+      if (res.status === 200) {
+        Object.keys(profile).forEach(key => {
+          profile[key].value = res.data[key]
+        })
+      }
+    })
+
+    return {
+      profile,
+    }
+  },
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+header {
+  @apply border-b border-gray-300 flex p-2 justify-end;
+
+  button {
+    @apply p-2 w-10 h-10;
+  }
+}
+
+.modal-body {
+  @apply mb-10 flex justify-center;
+
+  .profile-img-wrapper {
+    @apply bg-green-200 rounded-full w-32 h-32 flex items-center justify-center;
+
+    span {
+      @apply text-gray-600 font-medium;
+    }
+  }
+
+  .header {
+    @apply flex items-center justify-between mb-4;
+
+    h3 {
+      @apply text-xl font-bold;
+
+      .edit-btn {
+        @apply p-1 w-8 h-8;
+
+        .material-icons {
+          @apply text-gray-500;
+        }
+      }
+    }
+  }
+  .info-list {
+    @apply grid;
+
+    .info-list-item {
+      @apply flex items-center py-4 border-b;
+
+      .label {
+        @apply font-medium text-gray-400 w-40;
+      }
+    }
+  }
+  .change-pwd-btn-container {
+    @apply mt-4 flex justify-end;
+
+    button {
+      @apply font-medium text-sm text-gray-600;
+    }
+  }
+}
+</style>
