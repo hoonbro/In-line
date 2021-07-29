4b3c64b5d31b376c1bb9c424fb852b18f075322a
byunghun-jake <figma@kakao.com>
2021-07-29 17:30:03 +0900

    Update: vueConfig port 변경

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // publicPath: "/",
   devServer: {
     // https: true,
-    port: 8080,
+    port: 8083,
     proxy: {
       "/api/v1": {
         target: "http://13.124.47.223:8080",
