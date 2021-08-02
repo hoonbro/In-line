1d1442373e4e6e84c89a2ba5ebc240f66b8325f6
최영수 <soc4585@gmail.com>
2021-08-02 17:18:21 +0900

    Feat: 비밀번호 변경 페이지

--- Frontend/src/views/ChangePassword.vue
+++ Frontend/src/views/ChangePassword.vue
@@ -1,9 +1,79 @@
 <template>
-  <div>Change Password</div>
+  <div class="h-screen bg-gray-100 flex items-center justify-center">
+    <div
+      class="bg-white shadow-lg p-8 grid gap-8 rounded-lg"
+      style="width: 480px;"
+    >
+      <h1 class="text-3xl font-bold">비밀번호 변경</h1>
+      <div>
+        <p>
+          기존에 사용하던 비밀번호와 변경할 비밀번호를 입력하여, 비밀번호를
+          변경할 수 있어요.
+        </p>
+      </div>
+      <TextInput
+        v-for="(field, key) in formData"
+        :key="key"
+        :name="key"
+        v-model="field.value"
+        :field="field"
+        :formData="formData"
+      />
+      <button
+        class="bg-gray-400 text-white rounded-lg py-3"
+        @click="changePassword"
+      >
+        비밀번호 변경 완료
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import { reactive } from "vue"
+import TextInput from "@/components/TextInput.vue"
+import { requiredValidator, confirmPasswordValidator } from "@/lib/validator"
+
+export default {
+  name: "ChangePassword",
+  components: {
+    TextInput,
+  },
+  setup() {
+    const formData = reactive({
+      oldPassword: {
+        label: "이전 비밀번호",
+        type: "password",
+        value: "",
+        validators: [requiredValidator],
+        errors: {},
+      },
+      newPassword: {
+        label: "새 비밀번호",
+        type: "password",
+        value: "",
+        validators: [requiredValidator],
+        errors: {},
+      },
+      confirmPassword: {
+        label: "새 비밀번호 확인",
+        type: "password",
+        value: "",
+        validators: [requiredValidator, confirmPasswordValidator],
+        errors: {},
+      },
+    })
+
+    const changePassword = () => {
+      console.log(formData)
+    }
+
+    return {
+      formData,
+      changePassword,
+    }
+  },
+}
 </script>
 
 <style></style>
