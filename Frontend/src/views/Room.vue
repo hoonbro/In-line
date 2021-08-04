4c61c5bc06429dc76fbdd330b99bd4f830d789c9
최영수 <soc4585@gmail.com>
2021-08-04 16:55:06 +0900

    Update: participant 때려넣기

--- Frontend/src/views/Room.vue
+++ Frontend/src/views/Room.vue
@@ -77,7 +77,7 @@
 import Video from "@/components/Room/Video.vue"
 import { computed, onMounted, reactive, ref } from "@vue/runtime-core"
 import { useStore } from "vuex"
-import Participant from "@/lib/participant.js"
+// import Participant from "@/lib/participant.js"
 import kurentoUtils from "kurento-utils"
 
 export default {
@@ -276,6 +276,103 @@ export default {
       participant.dispose()
       delete participants[request.name]
     }
+
+    // ===========================================================================
+    // participant 시작
+    // ===========================================================================
+    const PARTICIPANT_MAIN_CLASS = "participant main"
+    const PARTICIPANT_CLASS = "participant"
+
+    /**
+     * Creates a video element for a new participant
+     *
+     * @param {String} name - the name of the new participant, to be used as tag
+     *                        name of the video element.
+     *                        The tag of the new element will be 'video<name>'
+     * @return
+     */
+
+    function Participant(name) {
+      this.name = name
+      let container = document.createElement("div")
+      // PARTICIPANT_MAIN_CLASS가 없을 때
+      container.className = isPresentMainParticipant()
+        ? PARTICIPANT_CLASS
+        : PARTICIPANT_MAIN_CLASS
+      container.id = name
+      let span = document.createElement("span")
+      let video = document.createElement("video")
+      let rtcPeer
+
+      container.appendChild(video)
+      container.appendChild(span)
+      container.onclick = switchContainerClass
+      document.getElementById("participants").appendChild(container)
+
+      span.appendChild(document.createTextNode(name))
+
+      video.id = "video-" + name
+      video.autoplay = true
+      video.controls = false
+
+      this.getElement = function() {
+        return container
+      }
+
+      this.getVideoElement = function() {
+        return video
+      }
+
+      function switchContainerClass() {
+        if (container.className === PARTICIPANT_CLASS) {
+          var elements = Array.prototype.slice.call(
+            document.getElementsByClassName(PARTICIPANT_MAIN_CLASS)
+          )
+          elements.forEach(function(item) {
+            item.className = PARTICIPANT_CLASS
+          })
+
+          container.className = PARTICIPANT_MAIN_CLASS
+        } else {
+          container.className = PARTICIPANT_CLASS
+        }
+      }
+
+      function isPresentMainParticipant() {
+        return (
+          document.getElementsByClassName(PARTICIPANT_MAIN_CLASS).length != 0
+        )
+      }
+
+      this.offerToReceiveVideo = function(error, offerSdp, wp) {
+        if (error) return console.error("sdp offer error")
+        console.log("Invoking SDP offer callback function")
+        var msg = { id: "receiveVideoFrom", sender: name, sdpOffer: offerSdp }
+        sendMessage(msg)
+      }
+
+      this.onIceCandidate = function(candidate, wp) {
+        console.log("Local candidate" + JSON.stringify(candidate))
+
+        var message = {
+          id: "onIceCandidate",
+          candidate: candidate,
+          name: name,
+        }
+        sendMessage(message)
+      }
+
+      Object.defineProperty(this, "rtcPeer", { writable: true })
+
+      this.dispose = function() {
+        console.log("Disposing participant " + this.name)
+        this.rtcPeer.dispose()
+        container.parentNode.removeChild(container)
+      }
+    }
+    // ===========================================================================
+    // participant 끝
+    // ===========================================================================
 
     // -------------------------------------------------------------------------------
     const videoList = 6
