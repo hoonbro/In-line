1af986a99c2dc062278bdd8681d2f35c420d4a26
Jihoon Byun <wlgns1588@naver.com>
2021-08-05 21:42:12 +0900

    Feat: profile upload

--- Backend/src/main/java/com/inline/sub2/Sub2Application.java
+++ Backend/src/main/java/com/inline/sub2/Sub2Application.java
@@ -3,9 +3,15 @@ package com.inline.sub2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class Sub2Application {
-
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(Sub2Application.class, args);
     }
--- Backend/src/main/java/com/inline/sub2/auth/JwtAuthenticationFilter.java
+++ Backend/src/main/java/com/inline/sub2/auth/JwtAuthenticationFilter.java
@@ -38,7 +38,6 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
         // If header is null delegate to Spring impl and exit
         if (header == null) {
-            log.info("토큰이 없습니다.");
             filterChain.doFilter(request, response);
             return;
         }
