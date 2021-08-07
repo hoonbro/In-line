dd499de222ff10c932ba79ce0a86cc3498795389
multicampus <sun950903@gmail.com>
2021-08-07 16:51:51 +0900

    Update: Chat일떄만 DB저장하기

--- Backend/src/main/java/com/inline/sub2/config/WebSocketConfig.java
+++ Backend/src/main/java/com/inline/sub2/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.inline.sub2.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,7 +12,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker //websocket 활성화
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-//    private final com.inline.sub2.api.handler.StompHandler stompHandler;
+//    private final com.inline.sub2.handler.StompHandler stompHandler;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
