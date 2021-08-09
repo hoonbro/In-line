62836b862882ded2de6c5a9f34c6dbbbeed31502
변지훈 <wlgns1588@naver.com>
2021-08-09 15:34:16 +0900

    Update: admin commute 수정

--- Backend/src/main/java/com/inline/sub2/db/repository/CommuteRepository.java
+++ Backend/src/main/java/com/inline/sub2/db/repository/CommuteRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Date;
 
 @Repository
 public interface CommuteRepository extends JpaRepository<CommuteEntity,String> {
