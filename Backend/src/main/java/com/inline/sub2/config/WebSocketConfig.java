d7e58336720705940393947a944f5e96c064566f
multicampus <sun950903@gmail.com>
2021-08-10 21:36:57 +0900

    Feat: 초대알림 구현중

--- Backend/src/main/java/com/inline/sub2/config/WebSocketConfig.java
+++ Backend/src/main/java/com/inline/sub2/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // "/sub" 시작되는 메시지가 메시지 브로커로 라우팅 되도록 정의
         //  메시지 브로커는 특정 주제를 구독 한 연결된 모든 클라이언트에게 메시지를 broadcast 한다.
 //        registry.setApplicationDestinationPrefixes("/pub");
-        registry.enableSimpleBroker("/sub");
+        registry.enableSimpleBroker("/sub","/queue");
     }
 
     @Override
