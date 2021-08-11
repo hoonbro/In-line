256f8f39cf715ef971c83d9b26a2feeb9d24acf7
김병훈 <figma@kakao.com>
2021-08-11 19:19:19 +0900

    Feat: 전역 컴포넌트 등록

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -5,9 +5,20 @@ import store from "./store"
 import "./assets/tailwind.css"
 import AOS from "aos"
 import "aos/dist/aos.css"
+import Loading from "@/components/Common/Loading.vue"
+import ConfirmModal from "@/components/Common/ConfirmModal.vue"
+import TextInput from "@/components/TextInput.vue"
+import SelectInput from "@/components/SelectInput.vue"
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .use(AOS)
-  .mount("#app")
+const app = createApp(App)
+
+// 컴포넌트 전역 등록
+app.component("ConfirmModal", ConfirmModal)
+app.component("TextInput", TextInput)
+app.component("SelectInput", SelectInput)
+app.component("Loading", Loading)
+
+app.use(store)
+app.use(router)
+app.use(AOS)
+app.mount("#app")
