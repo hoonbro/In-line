087be9bee929e2e4b9b693382f043558458169a1
변지훈 <wlgns1588@naver.com>
2021-08-12 14:06:45 +0900

    Feat: 회사별 인원 관리 정보 API 추가

--- Backend/src/main/java/com/inline/sub2/db/repository/UserRepository.java
+++ Backend/src/main/java/com/inline/sub2/db/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.inline.sub2.db.repository;
 
+import com.inline.sub2.api.dto.DeptUserDto;
 import com.inline.sub2.db.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
@@ -17,4 +19,9 @@ public interface UserRepository extends JpaRepository<UserEntity, String> {
 
     @Query("select ((select count(user.userId) from UserEntity user where user.retireDate is not null and user.officeId=?1) / 2) * 100 from UserEntity u where u.officeId = ?1")
     public Double findRetireRate(Long officeId);
+
+    public int countByOfficeId(long OfficeId);
+
+    @Query("select new com.inline.sub2.api.dto.DeptUserDto(d.deptName, count(u.userId)) from UserEntity u join DeptEntity d on u.deptId = d.deptId where u.officeId = :officeId group by u.deptId")
+    public List<DeptUserDto> countGroupByDept(long officeId);
 }
