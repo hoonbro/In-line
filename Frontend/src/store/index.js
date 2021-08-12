ce78dbab97dffa219876d0cd0e86bb4aedf4e69a
leeww0598 <leeww0598@gmail.com>
2021-08-12 21:35:55 +0900

    Feat: Admin Page

--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -4,7 +4,8 @@ import { office } from "@/store/modules/office"
 import { room } from "@/store/modules/room"
 import { auth } from "@/store/modules/auth"
 import { socket } from "@/store/modules/socket"
+import { admin } from "@/store/modules/admin"
 
 export default createStore({
-  modules: { landing, office, room, auth, socket },
+  modules: { landing, office, room, auth, socket, admin },
 })
