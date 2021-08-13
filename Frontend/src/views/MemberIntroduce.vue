bd6422b67de1c074c43d6ff08c6ed6c7066b6572
leeww0598 <leeww0598@gmail.com>
2021-08-14 00:35:35 +0900

    Feat: Member introduce

--- Frontend/src/views/MemberIntroduce.vue
+++ Frontend/src/views/MemberIntroduce.vue
@@ -1,13 +1,202 @@
 <template>
-  <section class="mt-16">
-    <div></div>
-    <div class="wrapper">
-      <div class="box"><p>선명준</p></div>
-      <div class="box"><p>변지훈</p></div>
-      <div class="box"><p>김병훈</p></div>
-      <div class="box"><p>조여래</p></div>
-      <div class="box"><p>최영수</p></div>
-      <div class="box"><p>이원우</p></div>
+  <section class="top">
+    <div class="intro text-center">
+      <p>영수님</p>
+      <p>영수님이 이 글을 읽고 있을 때</p>
+      <p>아마 저는 자고있겠지요</p>
+      <p>반응형으로 웹페이지를 만들려면</p>
+      <p>가장 작은 사이즈부터 만들라고 하십니다</p>
+      <p>영수님</p>
+      <p>화이팅!</p>
+      <p>기본 골자는</p>
+      <p>이렇게 가시면 될 것 같습니다</p>
+      <p>이름같은 경우는 바꾸셔도 됩니다</p>
+      <p>그럼 이만</p>
+      <p>하하</p>
+      <p>하하</p>
+      <p>하하</p>
+    </div>
+  </section>
+  <section class="bottom">
+    <div class="box">
+      <div class="box-item">
+        <div class="box-up p-4 flex">
+          <div><img src="@/assets/test/spin.gif" alt="" /></div>
+          <div>
+            <p class="explain">이름</p>
+            <p class="explain">나이</p>
+            <p class="explain">역할</p>
+            <p class="explain">MBTI</p>
+          </div>
+        </div>
+        <div class="box-down">
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/github.png" alt=""
+            /></a>
+          </div>
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/email.png" alt=""
+            /></a>
+          </div>
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/instagram.png" alt=""
+            /></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <div class="box-item">
+        <div class="box-up p-4 flex">
+          <div><img src="@/assets/test/spin.gif" alt="" /></div>
+          <div>
+            <p class="explain">이름</p>
+            <p class="explain">나이</p>
+            <p class="explain">역할</p>
+            <p class="explain">MBTI</p>
+          </div>
+        </div>
+        <div class="box-down">
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/github.png" alt=""
+            /></a>
+          </div>
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/email.png" alt=""
+            /></a>
+          </div>
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/instagram.png" alt=""
+            /></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <div class="box-item">
+        <div class="box-up p-4 flex">
+          <div><img src="@/assets/test/spin.gif" alt="" /></div>
+          <div>
+            <p class="explain">이름</p>
+            <p class="explain">나이</p>
+            <p class="explain">역할</p>
+            <p class="explain">MBTI</p>
+          </div>
+        </div>
+        <div class="box-down">
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/github.png" alt=""
+            /></a>
+          </div>
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/email.png" alt=""
+            /></a>
+          </div>
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/instagram.png" alt=""
+            /></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <div class="box-item">
+        <div class="box-up p-4 flex">
+          <div><img src="@/assets/test/spin.gif" alt="" /></div>
+          <div>
+            <p class="explain">이름</p>
+            <p class="explain">나이</p>
+            <p class="explain">역할</p>
+            <p class="explain">MBTI</p>
+          </div>
+        </div>
+        <div class="box-down">
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/github.png" alt=""
+            /></a>
+          </div>
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/email.png" alt=""
+            /></a>
+          </div>
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/instagram.png" alt=""
+            /></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <div class="box-item">
+        <div class="box-up p-4 flex">
+          <div><img src="@/assets/test/spin.gif" alt="" /></div>
+          <div>
+            <p class="explain">이름</p>
+            <p class="explain">나이</p>
+            <p class="explain">역할</p>
+            <p class="explain">MBTI</p>
+          </div>
+        </div>
+        <div class="box-down">
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/github.png" alt=""
+            /></a>
+          </div>
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/email.png" alt=""
+            /></a>
+          </div>
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/instagram.png" alt=""
+            /></a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="box">
+      <div class="box-item">
+        <div class="box-up p-4 flex">
+          <div><img src="@/assets/test/spin.gif" alt="" /></div>
+          <div>
+            <p class="explain">이름</p>
+            <p class="explain">나이</p>
+            <p class="explain">역할</p>
+            <p class="explain">MBTI</p>
+          </div>
+        </div>
+        <div class="box-down">
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/github.png" alt=""
+            /></a>
+          </div>
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/email.png" alt=""
+            /></a>
+          </div>
+          <div>
+            <a href=""
+              ><img class="icon" src="@/assets/Member/instagram.png" alt=""
+            /></a>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
@@ -19,12 +208,36 @@ export default {
 </script>
 
 <style scoped lang="scss">
-.wrapper {
-  @apply grid grid-cols-2 gap-16 mx-60;
+.top {
+  @apply bg-red-50 mx-64 my-36 border-4;
+  height: 50vh;
+}
+
+.bottom {
+  @apply grid grid-cols-2 mx-20;
   .box {
-    @apply bg-blue-100;
-    height: 50vh;
-    border: 2px solid red;
+    @apply w-3/5 mx-auto mb-6;
+    .box-item {
+      @apply bg-blue-100;
+      height: 55vh;
+      border: 2px solid red;
+      .box-up {
+        img {
+          @apply rounded-2xl h-80 w-60;
+        }
+
+        .explain {
+          @apply py-4 pl-4 text-5xl;
+        }
+      }
+      .box-down {
+        @apply grid grid-cols-3 mt-6;
+
+        .icon {
+          @apply w-24 h-24  mx-auto;
+        }
+      }
+    }
   }
 }
 </style>
