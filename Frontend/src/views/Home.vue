2ff8612c8e78a9de123e39d3781b2630f997feb4
byunghun-jake <figma@kakao.com>
2021-08-17 17:12:05 +0900

    Update: Landing Main 반응형 적용

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -7,19 +7,25 @@
         src="@/assets/LandingPage/logo1.png"
         alt="in-line logo"
       />
-      <h1 class="text-4xl mx-auto">선 안에 있는, 사람을 잇는</h1>
+      <h1 class="text-xl font-bold mx-auto">선 안에 있는, 사람을 잇는</h1>
     </div>
   </section>
   <!--///////////////////////////////////////////////////////////////////////-->
-  <section class="py-24 border-2 bg-gray-50">
-    <div class="font mb-12">
-      단일 회의실에 불과했던 기존의 <br />
-      가상회의 서비스를 개선한 웹 사무실 플랫폼입니다. <br />재택근무 소통
-      환경과 관리자의 편의성, <br />인-라인을 통해 업무 효율성을 높여보세요!
+  <section
+    class="flex flex-col bg-gray-50 items-center justify-center gap-10 py-20"
+  >
+    <div class="mx-4">
+      <p class="sub-text">
+        단일 회의실에 불과했던<br />
+        기존의 가상회의 서비스를 개선한<br />
+        <span class="text-blue-600">웹 사무실 플랫폼</span>입니다.<br />
+        재택근무 환경과 관리자의 편의성,<br />
+        <span class="text-blue-600">인라인</span>으로 업무 효율성을 높여보세요!
+      </p>
     </div>
-    <div class="flex justify-center gap-64">
+    <!-- <div class="flex justify-between w-full">
       <div class="icon">
-        <div class="icon-image w-24">
+        <div class="icon-image">
           <img src="@/assets/LandingPage/video-call.png" alt="화상 로고" />
         </div>
         <div class="icon-explain">
@@ -27,7 +33,7 @@
         </div>
       </div>
       <div class="icon">
-        <div class="icon-image w-24">
+        <div class="icon-image">
           <img src="@/assets/LandingPage/chat-box.png" alt="채팅 로고" />
         </div>
         <div class="icon-explain">
@@ -35,85 +41,208 @@
         </div>
       </div>
       <div class="icon">
-        <div class="icon-image w-24">
+        <div class="icon-image">
           <img src="@/assets/LandingPage/bar-chart.png" alt="인사이트 로고" />
         </div>
         <div class="icon-explain">
           <span></span>
         </div>
       </div>
-    </div>
+    </div> -->
   </section>
   <!--///////////////////////////////////////////////////////////////////////-->
-  <section class="section-box">
-    <div class="flex justify-center gap-4">
-      <div>
-        <h1>Home</h1>
-        <p class="item-text">
-          회의실 관리부터 근무 상태 파악까지 간편하게
+  <section class="content-section">
+    <div class="section__inner">
+      <header>
+        <h1>사무실</h1>
+        <p class="">
+          회의실 관리부터<br />
+          근무 상태 파악까지<br />
+          간편하게
         </p>
-      </div>
-      <div class="py-12 pl-4">
-        <img src="@/assets/LandingPage/home.png" alt="홈페이지 사진" />
+      </header>
+      <div class="img-container">
+        <img
+          class="lg:hidden first"
+          src="@/assets/LandingPage/office_mobile-1.jpg"
+          alt="홈페이지 사진1"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="bottom-center"
+        />
+        <img
+          class="lg:hidden second"
+          src="@/assets/LandingPage/office_mobile-2.jpg"
+          alt="홈페이지 사진2"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="bottom-center"
+        />
+        <img
+          class="hidden lg:block first"
+          src="@/assets/LandingPage/office_pc-1.jpg"
+          alt="홈페이지 사진1"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="bottom-center"
+        />
+        <img
+          class="hidden lg:block second"
+          src="@/assets/LandingPage/office_pc-2.jpg"
+          alt="홈페이지 사진2"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="bottom-center"
+        />
       </div>
     </div>
   </section>
   <!--///////////////////////////////////////////////////////////////////////-->
-  <section class="section-box bg-gray-50">
-    <div class="flex justify-center gap-4">
-      <div class="py-12 pr-4">
-        <img src="@/assets/LandingPage/members.png" alt="구성원페이지 사진" />
-      </div>
-      <div>
-        <h1>Members</h1>
+  <section class="content-section">
+    <div class="section__inner">
+      <header>
+        <h1>구성원</h1>
         <p class="item-text">
-          구성원 관리를 스마트하게
+          구성원 관리를<br />
+          스마트하게
         </p>
+      </header>
+      <div class="img-container">
+        <img
+          class="lg:hidden first"
+          src="@/assets/LandingPage/office_mobile-1.jpg"
+          alt="홈페이지 사진1"
+          data-aos="fade-right"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="bottom-center"
+        />
+        <img
+          class="lg:hidden second"
+          src="@/assets/LandingPage/office_mobile-2.jpg"
+          alt="홈페이지 사진2"
+          data-aos="fade-left"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="bottom-center"
+        />
+        <img
+          class="hidden lg:block first"
+          src="@/assets/LandingPage/office_pc-1.jpg"
+          alt="홈페이지 사진1"
+          data-aos="fade-right"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="bottom-center"
+        />
+        <img
+          class="hidden lg:block second"
+          src="@/assets/LandingPage/office_pc-2.jpg"
+          alt="홈페이지 사진2"
+          data-aos="fade-left"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="bottom-center"
+        />
       </div>
     </div>
   </section>
   <!--///////////////////////////////////////////////////////////////////////-->
-  <section class="section-box">
-    <div class="flex justify-center gap-4">
-      <div>
-        <h1>Admin</h1>
-        <p class="item-text">인사이트 파악과 근태 관리를 손쉽게</p>
-      </div>
-      <div class="py-12 pl-4">
-        <img src="@/assets/LandingPage/home.png" alt="홈페이지 사진" />
-      </div>
-    </div>
-  </section>
-  <!--///////////////////////////////////////////////////////////////////////-->
-  <footer class="bg-gray-200">
-    <div class="flex mx-auto justify-center gap-24">
-      <div>
-        <img class="my-10" src="@/assets/LandingPage/logo2.png" alt="" />
-      </div>
-      <div class="my-auto">
+  <section class="bg-gray-50 content-section">
+    <div class="section__inner">
+      <header>
+        <h1>관리</h1>
         <p>
-          ⓒ 인-라인 | 070-1234-5678 |
+          인사이트 파악과<br />
+          근태 관리를 손쉽게
+        </p>
+      </header>
+      <div class="img-container">
+        <img
+          class="lg:hidden first"
+          src="@/assets/LandingPage/office_mobile-1.jpg"
+          alt="홈페이지 사진1"
+          data-aos="fade-up-right"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="bottom-center"
+        />
+        <img
+          class="lg:hidden second"
+          src="@/assets/LandingPage/office_mobile-2.jpg"
+          alt="홈페이지 사진2"
+          data-aos="fade-up-left"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="bottom-center"
+        />
+        <img
+          class="hidden lg:block first"
+          src="@/assets/LandingPage/office_pc-1.jpg"
+          alt="홈페이지 사진1"
+          data-aos="fade-up-right"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="bottom-center"
+        />
+        <img
+          class="hidden lg:block second"
+          src="@/assets/LandingPage/office_pc-2.jpg"
+          alt="홈페이지 사진2"
+          data-aos="fade-up-left"
+          data-aos-duration="1000"
+          data-aos-anchor-placement="bottom-center"
+        />
+      </div>
+    </div>
+  </section>
+  <!--///////////////////////////////////////////////////////////////////////-->
+  <footer class="bg-gray-200 py-10">
+    <div class="flex flex-col mx-4 justify-center gap-6">
+      <div class="w-32">
+        <img
+          class="w-full h-full object-contain"
+          src="@/assets/LandingPage/logo2.png"
+          alt=""
+        />
+      </div>
+      <div class="grid gap-1">
+        <!-- <p>ⓒ 인-라인</p> -->
+        <p>070-1234-5678</p>
+        <p>
           <a
             href="mailto:iamlegend_@naver.com?subject=인라인에 문의하기&body=필요한 사항이 있다면 메일을 보내주세요."
-            >iamlegend_@naver.com</a
           >
+            iamlegend_@naver.com
+          </a>
         </p>
-        <p>
-          사업자 등록 번호 123-123-123 | 서비스 이용 약관 | 개인정보 처리 방침
-        </p>
+        <p>사업자 등록번호: 123-123-123</p>
+        <p>서비스 이용약관</p>
+        <p>개인정보 처리방침</p>
       </div>
     </div>
   </footer>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity"
+import AOS from "aos"
+
 export default {
   name: "Home",
+  setup() {
+    AOS.init()
+
+    return {}
+  },
 }
 </script>
 
 <style scoped lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;700&display=swap");
+/* image animation */
+.img-enter-from,
+.img-leave-to {
+  transform: translateY(40px);
+  @apply opacity-0;
+}
+
+.img-enter-active,
+.img-leave-active {
+  @apply transition-all duration-1000 delay-100;
+}
 
 .background-img {
   position: relative;
@@ -129,43 +258,59 @@ export default {
     z-index: 1;
   }
   .content {
-    position: absolute;
-    top: 50%;
-    left: 50%;
     transform: translate(-50%, -50%);
-    font-size: 5rem;
-    color: white;
-    z-index: 2;
-    text-align: center;
+    @apply absolute top-1/2 left-1/2 text-7xl text-white z-10 text-center w-full;
   }
 }
 
-.font {
-  @apply text-center text-4xl;
-  font-family: "Noto Sans KR", sans-serif;
-  font-weight: 600;
-  line-height: 8vh;
+.sub-text {
+  word-break: keep-all;
+  white-space: pre-wrap;
+  @apply text-center font-bold text-xl;
 }
+
 .icon {
   .icon-image {
-    @apply mx-auto mb-4;
+    @apply mx-auto w-20 h-20;
   }
   .icon-explain {
     @apply mx-10 text-2xl;
   }
 }
 
-.section-box {
-  @apply py-24 px-12;
+.content-section {
+  @apply flex flex-col gap-4 py-20 lg:py-40 h-screen;
 
-  h1 {
-    @apply text-5xl mb-4 text-blue-800 font-bold;
-  }
-  .item-text {
-    @apply w-52 text-3xl leading-10 font-bold;
-  }
-  img {
-    @apply border-t-2 border-r-2 shadow-md rounded-xl;
+  .section__inner {
+    @apply mx-4 grid gap-10;
+
+    header {
+      @apply grid gap-2;
+
+      h1 {
+        @apply text-blue-600 text-xl font-bold;
+      }
+
+      p {
+        @apply text-2xl font-bold md:text-3xl;
+      }
+    }
+
+    .img-container {
+      @apply relative;
+
+      img {
+        @apply max-w-xs md:max-w-md lg:max-w-xl xl:max-w-3xl 2xl:max-w-4xl shadow-2xl;
+      }
+
+      img.first {
+        @apply h-96 md:h-auto absolute -top-6 md:-top-32 right-0;
+      }
+
+      img.second {
+        @apply h-96 md:h-auto absolute top-10 left-0;
+      }
+    }
   }
 }
 </style>
